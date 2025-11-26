12d2e885d3e9bb803864b295d1bed7a7817e7757
Kurokishi592 <kennethwongcunwi@gmail.com>
2025-11-26 20:03:16 +0800

    post finals cleaned

--- lect tut demo/EE2213-Ultimate/README.md
+++
@@ -1,2 +0,0 @@
-# EE2213-Ultimate
-Includes all algorithms taught in the course. Refer to main.py's comments. Algorithms are in chronological order of them being taught.
--- lect tut demo/EE2213-Ultimate/dfs_recursive.py
+++
@@ -1,8 +0,0 @@
-# Recursive DFS function
-def dfs_recursive(graph, node, visited):
-    print(node)
-    visited.append(node)
-    for neighbour in graph.get(node, []):
-        if neighbour not in visited:
-            dfs_recursive(graph, neighbour, visited)
-    return visited
